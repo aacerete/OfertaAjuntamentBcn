b4c7f0fdda09b0fd462dd7b4ca9afe2586cadd11
aacerete <46990527d@iespoblenou>
2017-01-09 17:33:05 +0100

    Definim la vista del fragment per a realitzar les cerques amb dos Spinners i un button. Creem una nova activity que es generará quan fem lacerca

--- app/src/main/java/com/example/a46990527d/ofertaajuntamentbcn/MainActivityFragment.java
+++ app/src/main/java/com/example/a46990527d/ofertaajuntamentbcn/MainActivityFragment.java
@@ -1,11 +1,15 @@
 package com.example.a46990527d.ofertaajuntamentbcn;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 /**
@@ -36,15 +40,41 @@ public class MainActivityFragment extends Fragment {
                 {"Tots", "Concurs", "Lliure designació", "Mobilitat horitzontal", "Provisió oberta a altres administracions", "Oferta pública", "Promoció interna especial", "Borses de treball",
                  "Altres processos directius"   };
 
+        //Spinner 1
         Spinner spEns = (Spinner) view.findViewById(R.id.spinnerEns);
         ArrayAdapter<String> ensAdapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, ensValues);
         ensAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
         spEns.setAdapter(ensAdapter);
 
+
+        //Spinner 2
         Spinner spConcurs = (Spinner) view.findViewById(R.id.spinnerConcurs);
         ArrayAdapter<String> concursAdapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item, concursValues);
         ensAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
         spConcurs.setAdapter(concursAdapter);
+
+
+        //button
+
+        Button btnCercar = (Button) view.findViewById(R.id.btnCercar);
+        btnCercar.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.btnCercar:
+                        //what to put here
+
+                        Intent i = new Intent(getContext(), ResultadoActivity.class);
+                        i.putExtra("ajuntament", "tots");
+                        i.putExtra("concurs", "tots");
+                        startActivity(i);
+
+                 
+                        break;
+                }
+            }
+        });
 
         return view;
     }
