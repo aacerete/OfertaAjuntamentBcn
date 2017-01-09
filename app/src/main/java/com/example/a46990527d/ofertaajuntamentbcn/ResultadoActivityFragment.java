74c0c61f6fa52d75e76512dc49b06c1194fb8091
aacerete <46990527d@iespoblenou>
2017-01-09 17:55:01 +0100

    Creem un string amb ofertes d'exemple y la'adapter per al listview

--- app/src/main/java/com/example/a46990527d/ofertaajuntamentbcn/ResultadoActivityFragment.java
+++ app/src/main/java/com/example/a46990527d/ofertaajuntamentbcn/ResultadoActivityFragment.java
@@ -5,12 +5,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class ResultadoActivityFragment extends Fragment {
+
+    private ArrayList items;
+    private ArrayAdapter<String> adapter;
+
 
     public ResultadoActivityFragment() {
     }
@@ -22,6 +30,20 @@ public class ResultadoActivityFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
 
         ListView lvConcursos = (ListView) view.findViewById(R.id.lvConcursos);
+
+
+        String[] data = {"oferta 1", "oferta 2", "oferta 3", "oferta 4", "oferta 5", "oferta 6",};
+
+                items = new ArrayList<>(Arrays.asList(data));
+
+        adapter = new ArrayAdapter<>(
+                getContext(),
+                R.layout.lv_concursos_row,
+                R.id.tvEns,
+                items
+        );
+
+        lvConcursos.setAdapter(adapter);
 
         return view;
     }
