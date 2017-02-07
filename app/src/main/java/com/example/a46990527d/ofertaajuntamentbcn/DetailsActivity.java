88bd787a657c390e5933901c20a4d949298552b8
aacerete <46990527d@iespoblenou.org>
2017-02-07 07:24:47 +0100

    Millora de la pantalla d inici

--- app/src/main/java/com/example/a46990527d/ofertaajuntamentbcn/DetailsActivity.java
+++ app/src/main/java/com/example/a46990527d/ofertaajuntamentbcn/DetailsActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 
 import nl.littlerobots.cupboard.tools.provider.UriHelper;
 
+import static android.R.drawable.star_big_off;
 import static android.R.drawable.star_big_on;
 import static java.security.AccessController.getContext;
 import static nl.qbusict.cupboard.CupboardFactory.cupboard;
@@ -29,26 +30,62 @@ public class DetailsActivity extends AppCompatActivity {
         final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 
         Intent i = getIntent();
+
         if (i != null) {
+
             seleccio = (Selection) i.getSerializableExtra("seleccio");
+        }
+
+        if (seleccio != null) {
+            UriHelper helper = UriHelper.with(OfertaAjuntamentBcnContentProvider.AUTHORITY);
+            Uri selectionUri = helper.getUri(Selection.class);
+
+
+            Selection getdb = cupboard().withContext(getApplicationContext()).query(selectionUri, Selection.class).withSelection("numExpedient = ?", seleccio.getNumExpedient()).get();
+            //si la selecció no es troba a la BBDD
+            if (getdb == null) {
+                //button favorits off
+                fab.setImageResource(star_big_off);
+                //button favorits ON
+            } else {
+                fab.setImageResource(star_big_on);
+            }
         }
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(seleccio != null) {
+
+                if (seleccio != null) {
                     UriHelper helper = UriHelper.with(OfertaAjuntamentBcnContentProvider.AUTHORITY);
                     Uri selectionUri = helper.getUri(Selection.class);
-                    cupboard().withContext(getApplicationContext()).put(selectionUri, Selection.class, seleccio);
 
-                    Snackbar.make(view, "Convocatoria afegida a favorites", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
+                    //identifiquem les seleccions a partir del numExpedient que es únic
+                    Selection getdb = cupboard().withContext(getApplicationContext()).query(selectionUri, Selection.class).withSelection("numExpedient = ?", seleccio.getNumExpedient()).get();
 
-                    fab.setImageResource(star_big_on);
+                    //si es troba a la BBDD
+                    if (getdb != null) {
+                        //la esborrem i desmarquem el button
+                        cupboard().withContext(getApplicationContext()).delete(selectionUri,"numExpedient = ?",seleccio.getNumExpedient());
+
+                        Snackbar.make(view, "Convocatoria eliminada de favorites", Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
+
+                        fab.setImageResource(star_big_off);
+
+
+                    } else {
+                        //la afegim a favorites i a la BBDD
+                        cupboard().withContext(getApplicationContext()).put(selectionUri, Selection.class, seleccio);
+
+                        Snackbar.make(view, "Convocatoria afegida a favorites", Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
+
+                        fab.setImageResource(star_big_on);
+                    }
                 }
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
-
 }
