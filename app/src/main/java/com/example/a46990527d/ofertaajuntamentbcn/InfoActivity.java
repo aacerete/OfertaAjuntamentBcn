9edaf0d842495b610c11415c77faea134d994d5d
aacerete <46990527d@iespoblenou.org>
2017-02-07 17:37:04 +0100

    Activity amb informació. APP Finalitzada

---
+++ app/src/main/java/com/example/a46990527d/ofertaajuntamentbcn/InfoActivity.java
@@ -0,0 +1,16 @@
+package com.example.a46990527d.ofertaajuntamentbcn;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+
+public class InfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
--- app/src/main/java/com/example/a46990527d/ofertaajuntamentbcn/MainActivity.java
+++ app/src/main/java/com/example/a46990527d/ofertaajuntamentbcn/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.a46990527d.ofertaajuntamentbcn;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -60,6 +61,8 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent i = new Intent(getApplicationContext(),InfoActivity.class);
+            startActivity(i);
             return true;
         }
 
